dd0bd576e78fe6acf0492ac27a2bea57b2880a78
Mayank Aggarwal <mayankagg9722@gmail.com>
2016-08-08 01:54:05 +0530

    Use Spinner

---
+++ Spinner/app/src/main/java/com/example/mayank/spinner/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.mayank.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner=(Spinner)findViewById(R.id.spinner);
+        adapter=ArrayAdapter.createFromResource(this,R.array.country,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),"You have selected"+" "+parent.getItemAtPosition(position),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+    }
